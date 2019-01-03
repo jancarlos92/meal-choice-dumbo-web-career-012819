5492b54bd97e510a526fd6350872f493b2436824
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-03 19:36:36 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,6 @@
-# Define a method meal_choice that returns the meal_choice that was
-# passed into it and defaults to meat.
+def meal_choice(choice = "meat")
+  puts "you will be having #{choice} today!"
+end
+
+meal_choice("chicken")
+
